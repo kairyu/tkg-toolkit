f39f3bdcaf65669dc0bbd77c1f108ae084dd998f
Kai Ryu <kai1103@gmail.com>
2013-12-17 23:31:17 +0900

    Import files

---
+++ reflash-gh60.bat
@@ -0,0 +1,3 @@
+@echo off
+cd %~dp0
+reflash.bat gh60 %*
---
+++ reflash.bat
@@ -0,0 +1,22 @@
+@echo off
+cd %~dp0
+set PROGRAMMER=dfu-programmer
+set TARGET=atmega32u4
+set HEX=hex\%1.hex
+if not exist %HEX% goto :END
+set "EEP="
+shift
+:LOOP
+if not "%~1" == "" (
+	set "EEP=%EEP% %1"
+	shift
+	goto :LOOP
+)
+@echo on
+%PROGRAMMER% %TARGET% erase
+%PROGRAMMER% %TARGET% flash %HEX%
+@if "%EEP%" == "" goto :END
+%PROGRAMMER% %TARGET% flash-eeprom "%EEP%"
+:END
+%PROGRAMMER% %TARGET% start
+pause
