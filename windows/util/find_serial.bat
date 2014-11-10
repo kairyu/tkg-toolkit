116c99a2fca53fe85d44aa484d88259c4fdc0b7b
Kai Ryu <kai1103@gmail.com>
2014-11-10 19:31:24 +0900

    Add a utility for finding serial port of arduino

---
+++ windows/util/find_serial.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+set FIRST=1
+set "PREV="
+set "COM="
+echo | set /p=Finding...
+
+:LOOP
+set "LIST="
+for /f "DELIMS=" %%a in ('mode') do (
+	set OUT=%%a
+	set "TEST=!OUT:COM=!"
+	if not "!TEST!"=="!OUT!" (
+		set PORT=!OUT:*COM=!
+		set PORT=!PORT::=!
+		set "LIST=!LIST! !PORT!"
+	)
+)
+if not "%FIRST%" == "1" (
+	for %%a in (%LIST%) do (
+		if not "%%a" == "" (
+			set FOUND=0
+			for %%b in (%PREV%) do (
+				if "!FOUND!" == "0" if %%a == %%b set FOUND=1
+			)
+			if "!FOUND!" == "0" (
+				set COM=COM%%a
+				goto :END
+			)
+		)
+	)
+)
+set FIRST=0
+set PREV=%LIST%
+goto :LOOP
+
+:END
+echo !COM!
+copy /y NUL !COM! >NUL
+pause
