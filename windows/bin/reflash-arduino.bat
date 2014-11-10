4b750b2ea7c1ba34d20a925fb601804c60d79d82
Kai Ryu <kai1103@gmail.com>
2014-11-10 16:41:23 +0900

    Improve features of reflash-arduino - Print usage - Reflash hex when call with hex argument - Reflash eep when call with eep or both hex and eep arguments - Reflash hex and replace existing hex when call with two hex arguments

--- windows/bin/reflash-arduino.bat
+++ windows/bin/reflash-arduino.bat
@@ -1,56 +1,113 @@
 @echo off
 setlocal enabledelayedexpansion
-cd %~dp0
-set EXEC=bin\avrdude
-set PARTNO=atmega32u4
+set SCRIPT=%~nx0
+set EXEC=%~dp0\avrdude
+if "%PARTNO%" == "" set PARTNO=atmega32u4
 set PROGRAMMER=avr109
-set "HEX=%~1"
-if not exist %HEX% goto :END
-shift
-set "EEP=%~1"
-shift
-:LOOP
-if not "%~1" == "" (
-	set "EEP=%EEP% %1"
-	shift
-	goto :LOOP
-)
+set "HEX="
+set "HEX_ORIG="
+set "EEP="
 
+set "ARG1=%~1"
+if "%ARG1%" == "" goto :USAGE
+if not exist %ARG1% goto :USAGE
+set /a ARGS=1
+shift
+
+set "ARG2=%~1"
+:ARG2_LOOP
+shift
+if not "%~1" == "" (
+	set "ARG2=%ARG2% %1"
+	goto :ARG2_LOOP
+)
+if not "%ARG2%" == "" set /a ARGS=2
+
+if "!ARGS!" == "1" (
+	if not exist %ARG1% goto :USAGE
+	for %%i in (%ARG1%) do set ARG1_EXT=%%~xi
+	if "!ARG1_EXT!" == ".hex" (
+		set HEX=%ARG1%
+		goto :REFLASH
+	)
+	if "!ARG1_EXT!" == ".eep" (
+		set EEP=%ARG1%
+		goto :REFLASH
+	)
+	goto :USAGE
+)
+if "!ARGS!" == "2" (
+	if not exist %ARG2% goto :USAGE
+	for %%i in (%ARG2%) do set ARG2_EXT=%%~xi
+	if "!ARG2_EXT!" == ".hex" (
+		set HEX=%ARG2%
+		set HEX_ORIG=%ARG1%
+		goto :REFLASH
+	)
+	if "!ARG2_EXT!" == ".eep" (
+		set HEX=%ARG1%
+		set EEP=%ARG2%
+		goto :REFLASH
+	)
+	goto :USAGE
+)
+goto :USAGE
+
+:REFLASH
 for %%f in (*) do (
 	set "FILE=%%f"
 	set "TEST=!FILE:COM=!"
-	if not "!TEST!"=="!FILE!" (
+	if not "!TEST!" == "!FILE!" (
 		set "COM=!FILE!"
 		echo Waiting for !COM!...
 		goto :COM
 	)
 )
 echo COM not specified
-goto END
+goto :END
 
 :COM
 for /f "DELIMS=" %%a in ('mode %COM%') do set OUT=%%a
 set "TEST=!OUT::=!"
-if not "!TEST!"=="!OUT!" goto FLASH
-goto COM
+if not "!TEST!" == "!OUT!" goto :FLASH
+goto :COM
 
 :FLASH
 echo %COM% is ready
 set PORT=%COM%
-if "%EEP%"=="" (
+if "%EEP%" == "" (
 	echo Reflashing HEX file...
 	%EXEC% -p%PARTNO% -c%PROGRAMMER% -P%PORT% -Uflash:w:"%HEX%":i
 )
-if not "%EEP%"=="" (
+if not "%EEP%" == "" (
 	echo Reflashing EEP file...
 	%EXEC% -p%PARTNO% -c%PROGRAMMER% -P%PORT% -Ueeprom:w:"%EEP%":i
 )
-if "%ERRORLEVEL%"=="0" (
-	echo Success^^!
-)
-if not "%ERRORLEVEL%"=="0" (
+if not "%ERRORLEVEL%" == "0" (
 	echo Fail^^!
 )
+if "%ERRORLEVEL%" == "0" (
+	echo Success^^!
+	if not "%HEX_ORIG%" == "" (
+		set "INPUT="
+		set /p INPUT=Replace existing HEX with the new one? [y/N]
+		if "!INPUT!" == "y" (
+			copy /y %HEX% %HEX_ORIG%
+			if not "%ERRORLEVEL%" == "0" (
+				echo Fail^^!
+			)
+			if "%ERRORLEVEL%" == "0" (
+				echo Success^^!
+			)
+		)
+	)
+)
+goto :END
+
+:USAGE
+@echo Usage: %SCRIPT% hex [hex^|eep]
+goto :END
 
 :END
-pause
+endlocal
+:: pause
