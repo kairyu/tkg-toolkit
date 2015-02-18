545f4f8abed8064a052fdb82f82906e127148db4
Kai Ryu <kai1103@gmail.com>
2015-02-18 18:17:26 +0900

    Migrate firmwares from kairyu/tkg-toolkit

--- windows/reflash-kimera_th_arduino.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd "%cd%"
-cd %~dp0
-set bat=%~n0
-set bat=%bat:_arduino=%
-set PARTNO=atmega32u4
-.\bin\reflash-arduino.bat ..\common\firmware\%bat:~8%.hex %*
-popd
-pause
--- windows/reflash-smart68.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pushd "%cd%"
-cd %~dp0
-set bat=%~n0
-set TARGET=atmega32u4
-.\bin\reflash-dfu.bat ..\common\firmware\%bat:~8%.hex %*
-popd
-pause
